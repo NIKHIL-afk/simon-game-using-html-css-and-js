e0a2d3584da44ee6ce962db4359b0d098bec43f0
NIKHIL-afk <69781085+NIKHIL-afk@users.noreply.github.com>
2021-07-03 14:03:11 +0530

    Add files via upload

---
+++ Simon Game using html css and JS/index.js
@@ -0,0 +1,97 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+
+var level = 0;
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("level " + level);
+    nextSequence(); //didnt knew why nextSequence is written before started
+    started = true;
+
+  }
+});
+
+function nextSequence() {
+userClickedPattern = [];
+  level++;
+
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+function animatePress(currentColor) {
+
+  //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+  $("#" + currentColor).addClass("pressed");
+
+  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+
+
+
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+
+
+  if (userClickedPattern.length === gamePattern.length){
+
+    //5. Call nextSequence() after a 1000 millisecond delay.
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+
+  }
+
+  }else {
+  var a=new Audio("sounds/wrong.mp3");
+  a.play();
+  $("body").addClass("game-over");
+  setTimeout(function(){$("body").removeClass("game-over")},200);
+    console.log("wrong");
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+  startOver();
+  }
+
+}
+
+
+function startOver(){
+  level=0;
+  gamePattern=[];
+  started=false;
+}
